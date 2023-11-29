ebddeea7ae6e2cff3ee673d6eea150ba3702061b
Matheus Andrade <matheuslincon50@gmail.com>
2023-11-29 23:03:19 +1100

    refactor: create mapping method in MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,7 +7,7 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    const account = { id: result.insertedId.toString(), ...accountData }
-    return account
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+    return MongoHelper.map(account)
   }
 }
